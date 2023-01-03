be92a9d041c0b204bd63ae0be87f5703d83fd083
tejj1221 <tejj1221@gmail.com>
2023-01-03 19:29:00 +0530

    added all files

---
+++ App.js
@@ -0,0 +1,27 @@
+import Addproperty from './components/addproperty/Addproperty';
+import './App.css';
+
+import Login from './components/Authenticate/login';
+import Register from './components/Authenticate/register';
+import Home from './components/home/home';
+import {BrowserRouter,Routes,Route} from "react-router-dom";
+import { useState } from 'react';
+function App() {
+  const[token,settoken]=useState("")
+  const[id,setid]=useState("")
+  const[name,setname]=useState("")
+  return (
+
+  <BrowserRouter>
+    <Routes>
+    <Route path="/" element={<Register/>}/>
+    <Route path="/login" element={<Login token={token} settoken={settoken} id={id} setid={setid} name={name} setname={setname}/>}/> 
+    <Route path='/property' element={<Home token={token} settoken={settoken} id={id} setid={setid} name={name}/>} />
+    <Route path="/property/addnew" element={<Addproperty token={token} settoken={settoken} id={id} setid={setid} name={name}/>}/> 
+  </Routes>
+  </BrowserRouter>
+    
+  )}
+
+
+export default App;
